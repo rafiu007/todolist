a6784526ad4fc3ae23a74def632ccca67da3f639
Rafiu Jaman Mollah <rafiu.jaman1997@gmail.com>
2020-02-09 12:21:27 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,59 @@
+
+//adding new element
+function newElement(){
+    var li=document.createElement("li");
+    var inputval=document.getElementById("myInput").value;
+    var t=document.createTextNode(inputval);
+    li.appendChild(t);
+    if(inputval===''){
+        alert("Enter something");
+    }
+    else{
+        document.getElementById("mylist").appendChild(li);
+    }
+
+    document.getElementById("myInput").value="";
+
+    var span=document.createElement("SPAN");
+    var txt=document.createTextNode("\u00D7");
+    span.className="close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for(i=0;i<close.length;i++){
+        close[i].onclick=function(){
+            var div=this.parentElement;
+            div.style.display="none";
+        }
+    }
+
+}
+
+
+//removing every item
+
+function removeAll(){
+    var del=document.getElementsByTagName("ul");
+    del[0].innerHTML="";
+}
+
+var close=document.getElementsByClassName("close");
+
+var i;
+
+
+// hiding a particular item
+
+for(i=0;i<close.length;i++){
+    close[i].onclick=function(){
+        var div=thi.parentElement;
+        div.style.display="none";
+    }
+}
+
+var list =document.querySelector('ul');
+list.addEventListener('click',function(ev){
+    if(ev.target.tagName==="LI"){
+        ev.target.classList.toggle('checked');
+    }
+},false);
